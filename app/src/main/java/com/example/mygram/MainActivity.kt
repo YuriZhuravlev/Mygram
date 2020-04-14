7208c2fc0583b0afad3f1ce9b85f72750b070021
Agafiy_Poluchlebnik <zhuravlevyuri2000@gmail.com>
2020-04-14 22:10:19 +0300

    color and items fixes

--- app/src/main/java/com/example/mygram/MainActivity.kt
+++ app/src/main/java/com/example/mygram/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.mygram
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.preference.PreferenceActivity
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import com.example.mygram.databinding.ActivityMainBinding
 import com.mikepenz.materialdrawer.AccountHeader
@@ -12,6 +14,7 @@ import com.mikepenz.materialdrawer.DrawerBuilder
 import com.mikepenz.materialdrawer.model.DividerDrawerItem
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem
 import com.mikepenz.materialdrawer.model.ProfileDrawerItem
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mBinding: ActivityMainBinding
@@ -62,41 +65,62 @@ class MainActivity : AppCompatActivity() {
                 PrimaryDrawerItem().withIdentifier(101)
                     .withIconTintingEnabled(true)
                     .withName(R.string.menu_item1)
+                    .withIcon(R.drawable.ic_menu_create_groups)
                     .withSelectable(false),
                 PrimaryDrawerItem().withIdentifier(102)
                     .withIconTintingEnabled(true)
                     .withName(R.string.menu_item2)
+                    .withIcon(R.drawable.ic_menu_secret_chat)
                     .withSelectable(false),
                 PrimaryDrawerItem().withIdentifier(103)
                     .withIconTintingEnabled(true)
                     .withName(R.string.menu_item3)
+                    .withIcon(R.drawable.ic_menu_create_channel)
                     .withSelectable(false),
                 PrimaryDrawerItem().withIdentifier(104)
                     .withIconTintingEnabled(true)
                     .withName(R.string.menu_item4)
+                    .withIcon(R.drawable.ic_menu_contacts)
                     .withSelectable(false),
                 PrimaryDrawerItem().withIdentifier(105)
                     .withIconTintingEnabled(true)
                     .withName(R.string.menu_item5)
+                    .withIcon(R.drawable.ic_menu_phone)
                     .withSelectable(false),
                 PrimaryDrawerItem().withIdentifier(106)
                     .withIconTintingEnabled(true)
                     .withName(R.string.menu_item6)
+                    .withIcon(R.drawable.ic_menu_favorites)
                     .withSelectable(false),
                 PrimaryDrawerItem().withIdentifier(107)
                     .withIconTintingEnabled(true)
                     .withName(R.string.menu_item7)
+                    .withIcon(R.drawable.ic_menu_settings)
                     .withSelectable(false),
                 DividerDrawerItem(),
                 PrimaryDrawerItem().withIdentifier(108)
                     .withIconTintingEnabled(true)
                     .withName(R.string.menu_item8)
+                    .withIcon(R.drawable.ic_menu_invate)
                     .withSelectable(false),
                 PrimaryDrawerItem().withIdentifier(109)
                     .withIconTintingEnabled(true)
                     .withName(R.string.menu_item9)
+                    .withIcon(R.drawable.ic_menu_help)
                     .withSelectable(false)
             )
+            .withOnDrawerItemClickListener(object :Drawer.OnDrawerItemClickListener{
+                override fun onItemClick(
+                    view: View?,
+                    position: Int,
+                    drawerItem: IDrawerItem<*>
+                ): Boolean {
+                    ///////////////////////////////////////
+                    Toast.makeText(applicationContext,position.toString(),Toast.LENGTH_SHORT).show()
+                    ///////////////////////////////////////
+                    return false
+                }
+            })
             .build()
     }
 }
