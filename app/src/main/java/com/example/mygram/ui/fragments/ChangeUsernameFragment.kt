980d227598e3f0cf3973060a4e2299179d347997
Agafiy_Poluchlebnik <zhuravlevyuri2000@gmail.com>
2020-04-21 20:56:00 +0300

    change Username

---
+++ app/src/main/java/com/example/mygram/ui/fragments/ChangeUsernameFragment.kt
@@ -0,0 +1,89 @@
+package com.example.mygram.ui.fragments
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import com.example.mygram.MainActivity
+
+import com.example.mygram.R
+import com.example.mygram.utilits.*
+import kotlinx.android.synthetic.main.fragment_change_username.*
+import java.util.*
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class ChangeUsernameFragment : BaseFragment(R.layout.fragment_change_username) {
+
+    lateinit var mUserName: String
+
+    override fun onResume() {
+        super.onResume()
+        setHasOptionsMenu(true)
+        settings_input_username.setText(USER.username)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        (activity as MainActivity).menuInflater.inflate(R.menu.settings_menu_confirm, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.settings_confirm_change -> change()
+        }
+        return true
+    }
+
+    private fun change() {
+        mUserName = settings_input_username.text.toString().toLowerCase(Locale.getDefault())
+        if (mUserName.isEmpty())
+            showToast("Поле пустое!")
+        else {
+            REF_DATABASE_ROOT.child(NODE_USERNAMES)
+                .addListenerForSingleValueEvent(AppValueEventListener {
+                    if (it.hasChild(mUserName)) {
+                        showToast(getString(R.string.settings_toast_username_exists))
+                    } else {
+                        changeUsername()
+                    }
+                })
+        }
+    }
+
+    private fun changeUsername() {
+        REF_DATABASE_ROOT.child(NODE_USERNAMES).child(mUserName).setValue(UID)
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    updateCurrentUsername()
+                } else {
+                    showToast(it.exception?.message.toString())
+                }
+            }
+    }
+
+    private fun updateCurrentUsername() {
+        REF_DATABASE_ROOT.child(NODE_USERS).child(UID).child(CHILD_USERNAME)
+            .setValue(mUserName)
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    deleteOldUsername()
+
+                } else {
+                    showToast(it.exception?.message.toString())
+                }
+            }
+    }
+
+    private fun deleteOldUsername() {
+        REF_DATABASE_ROOT.child(NODE_USERNAMES).child(USER.username).removeValue()
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    showToast(getString(R.string.toast_data_update))
+                    USER.username = mUserName
+                    fragmentManager?.popBackStack()
+                } else {
+                    showToast(it.exception?.message.toString())
+                }
+            }
+    }
+}
